dd2667fa745052e628988f79101251b9d6157050
wellinton523 <wellint5x@gmail.com>
2025-03-26 10:34:33 -0300

    Imphyria(site)

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const sidebar = document.querySelector(".sidebar");
 const sidebartoggler = document.querySelector(".sidebar-toggler");
 const devmenulist = document.querySelector(".dev-menu-list")
 const devmenutoggler = document.querySelector(".dev-toggler")
+const sinfoheader = document.querySelector(".s-info-header")
+const infoheader = document.querySelector(".info-background")
 
 window.onload = function() {
     sidebar.classList.toggle("collapsed");
@@ -10,6 +12,8 @@ window.onload = function() {
 
 sidebartoggler.addEventListener("click", () => {
     sidebar.classList.toggle("collapsed");
+    sinfoheader.classList.toggle("collapsed")
+    infoheader.classList.toggle("collapsed")
 })
 devmenutoggler.addEventListener("click", () => {
     devmenulist.classList.toggle("collapsed")
