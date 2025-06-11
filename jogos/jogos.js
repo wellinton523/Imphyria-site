a7fa47220e785adce388b44986d0ba1d6179978b
wellinton523 <wellint5x@gmail.com>
2025-06-10 22:11:03 -0300

    wayless

--- jogos/jogos.js
+++ jogos/jogos.js
@@ -2,6 +2,8 @@ const sidebar = document.querySelector(".sidebar");
 const sidebartoggler = document.querySelector(".sidebar-toggler");
 const devmenulist = document.querySelector(".dev-menu-list")
 const devmenutoggler = document.querySelector(".dev-toggler")
+const sinfoheader = document.querySelector(".s-info-header")
+const infoheader = document.querySelector(".info-background")
 const midiab = document.querySelector(".midia_button")
 const midialist = document.querySelector(".midia_div")
 
@@ -15,7 +17,9 @@ window.onload = function() {
 
 sidebartoggler.addEventListener("click", () => {
     sidebar.classList.toggle("collapsed");
+    sinfoheader.classList.toggle("collapsed")
     midialist.classList.add("collapsed")
+    infoheader.classList.toggle("collapsed")
     if (document.querySelector(".sidebar-text").textContent == "Wayless"){
         setTimeout(() => {
             document.querySelector(".sidebar-text").textContent = "Wayless Studios"
