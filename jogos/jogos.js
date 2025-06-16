de6b433bf261e595ecf424aef4c068178b620bad
wellinton523 <wellint5x@gmail.com>
2025-06-16 14:05:33 -0300

    update de padronizaçao e correçoes

--- jogos/jogos.js
+++ jogos/jogos.js
@@ -1,7 +1,5 @@
 const sidebar = document.querySelector(".sidebar");
 const sidebartoggler = document.querySelector(".sidebar-toggler");
-const devmenulist = document.querySelector(".dev-menu-list")
-const devmenutoggler = document.querySelector(".dev-toggler")
 const sinfoheader = document.querySelector(".s-info-header")
 const infoheader = document.querySelector(".info-background")
 const midiab = document.querySelector(".midia_button")
@@ -17,9 +15,7 @@ window.onload = function() {
 
 sidebartoggler.addEventListener("click", () => {
     sidebar.classList.toggle("collapsed");
-    sinfoheader.classList.toggle("collapsed")
     midialist.classList.add("collapsed")
-    infoheader.classList.toggle("collapsed")
     if (document.querySelector(".sidebar-text").textContent == "Wayless"){
         setTimeout(() => {
             document.querySelector(".sidebar-text").textContent = "Wayless Studios"
